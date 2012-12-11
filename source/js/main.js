2759caea9089c18ea2fac382c922812c7512935e
Christian Kruse <cjk@wwwtech.de>
2012-12-11 19:38:18 +0100

    now animating and changing the heading in different languages

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,13 @@
+$(function() {
+  var texts = ['Schweiz', 'Suisse', 'Suizzera', 'Svizra', 'Switzerland'];
+  var pos = 0;
 
+  window.setInterval(function() {
+    $("#caption-switzerland").fadeOut('fast', function() {
+      $(this).text(texts[++pos]);
+    });
+    $("#caption-switzerland").fadeIn('fast');
+  }, 3000);
+});
+
+// eof
